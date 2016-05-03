57a57551e12ba8dd5c0c1ae80b8ca1011294ae0a
Joseph Kane <josephckane@gmail.com>
2016-05-03 12:45:10 -0500

    finalize layout styling, small syntax tweaks to food.js

--- food.js
+++ food.js
@@ -3,35 +3,42 @@ var foodList = document.getElementById("main-content")
 
 function dogDataCall () {
 	var dogData = JSON.parse(this.responseText);
-	populateDOM(dogData);
+	populateDOM(dogData.dog_brands);
 }
 
 function catDataCall () {
 	var catData = JSON.parse(this.responseText);
-	populateDOM(catData)
+	populateDOM(catData.cat_brands)
 }
 
 function populateDOM (XHRdata) {
 	var HTML = "";
-	for (var i = 0; i < XHRdata.brands.length; i++) {
-		HTML = `<div class="brand"><h2 class="brand-headline">${XHRdata.brands[i].brand}</h2>`;
-		if (XHRdata.brands[i].brand === "Purrina" || XHRdata.brands[i].brand === "Meow Meal")	{
-			HTML += `<h3 class="breed-headline">Breed:</h3>`;
+	for (var i = 0; i < XHRdata.length; i++) {
+		// if (XHRdata[i].brand === "Chuck Wagon") {
+		// 	HTML = `<h1>For Dogs:</h1>`;
+		// } else if (XHRdata[i].brand === "Purrina") {
+		// 	HTML = `<h1>For Cats:</h1>`;
+		// };
+		HTML = `<div class="brand clearfix"><h2 class="brand-headline">${XHRdata[i].brand}</h2>`;
+		if (XHRdata[i].brand === "Purrina" || XHRdata[i].brand === "Meow Meal")	{
+			HTML += `<h3 class="breed-headline">Breeds:</h3>`;
 		};
-		var loopData = XHRdata.brands[i].types;
-		var breedArray = XHRdata.brands[i];
-		if (XHRdata.brands[i].breed) {
+		var loopData = XHRdata[i].types;
+		var breedArray = XHRdata[i];
+		if (XHRdata[i].breed) {
 			for (var l = 0; l < breedArray.breed.length; l++) {
 				HTML += `<p class="cat-breeds">${breedArray.breed[l]}</p>`
 			};
 		};
 		for (var j = 0; j < loopData.length; j++) {
-			HTML += `<h3 class="types">${loopData[j].type}</h3>`
+		HTML += `<div class="types">`;
+			HTML += `<h3 class="type-headline">${loopData[j].type}</h3>`
 			var arrayData = loopData[j].volumes;
+			HTML += `<div class="products"><ul>`;
 			for (var k = 0; k < arrayData.length; k++) {
-				HTML += `<div class="products"><ul><li>${arrayData[k].size}</li><li>${arrayData[k].price}</li></div>`;
-				console.log("html:", HTML);
+				HTML += `<li>${arrayData[k].size}: ${arrayData[k].price}</li>`;
 			} // k
+			HTML += `</ul></div></div>`;
 		} // j
 		foodList.innerHTML += (HTML += `</div>`);
 	} // i
